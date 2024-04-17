c6612287bda54974da270b89b007941cf1daa2fc
Savindu <savinduruhunuhewa@gmail.com>
2024-04-17 17:52:47 +0530

    Create the app.py and link the model

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import pickle
+app = Flask(__name__)
+
+
+# Change the path to the local path where your model is stored
+model_path = './DengueDetectionModel.h5'
+model = pickle.load(open(model_path, 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    feature_names = ['0', '1', '2', '3', '4', '5', '6','7', '8', '9', '10']
+
+    try:
+        features = [float(request.form[f]) for f in feature_names]
+    except ValueError:
+        return render_template('./pages/index.html', error="Invalid input. Please enter numeric values.")
+
+    input_data = [features]
+
+    # Make prediction
+    pred = model.predict(input_data)
+
+    # Display the result
+    if pred[0] == 0:
+        result = "Dengue is not infected"
+    else:
+        result = "Dengue is infected"
+
+    return render_template('index.html', result=result)
+
+    webview.create_window('Flask to exe',app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
