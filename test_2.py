2f0c453364e31dda93453182eef65905688a9e9f
Savindu <savinduruhunuhewa@gmail.com>
2024-04-23 11:32:41 +0530

    Complete the pytests

--- test_2.py
+++ test_2.py
@@ -1,26 +1,32 @@
 import pytest
 import requests
+from bs4 import BeautifulSoup
 
 # Define the URL of your Flask application
 BASE_URL = 'http://127.0.0.1:5000'
 
 # Sample input data for testing
 SAMPLE_INPUT_DATA = {
-    '0': '1', '1': '1', '2': '1', '3': '1', '4': '1', '5': '1', '6': '1', '7': '1', '8': '1', '9': '1', '10': '1'
+    '0': '1', '1': '0', '2': '0'
 }
 
-def test_predict_route(capsys):
+def test_predict_route():
     # Send a POST request to the predict route with sample input data
     response = requests.post(f'{BASE_URL}/dengueprediction', data=SAMPLE_INPUT_DATA)
-    
+    response = requests.post(f'{BASE_URL}/dengueprediction', data=SAMPLE_INPUT_DATA)
+    print(response.content)
+
     # Check if the response is successful (status code 200)
     assert response.status_code == 200
     
-    # Extract the result from the response text
-    #result = response.text.strip()  # Remove leading/trailing whitespace
+    # Parse the HTML response
+   # soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Print the result to the terminal
-    #print(result)
+    # Find the element containing the result
+    #result_element = soup.find(id='resultMessage')
+    
+    # Extract the result text
+    #result = result_element.get_text() if result_element else None
     
     # Check if the expected messages are present in the result
     #assert 'Dengue is infected' in result or 'Dengue is not infected' in result
