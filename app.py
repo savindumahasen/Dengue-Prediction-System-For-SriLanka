53b5329c51e012587cd32c9ba55e6b3a284b2177
Savindu <savinduruhunuhewa@gmail.com>
2024-05-03 15:55:36 +0530

    Remove the try catch block

--- app.py
+++ app.py
@@ -15,11 +15,8 @@ def home():
 def predict():
     feature_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-    try:
-        features = [float(request.form[f]) for f in feature_names]
-    except ValueError:
-        return jsonify({'error': 'Invalid input'})
-
+    features = [float(request.form[f]) for f in feature_names]
+ 
     input_data = [features]
 
     # Make prediction
